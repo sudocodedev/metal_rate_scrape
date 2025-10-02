f8fb8db9b13306849e73e5740fe3a10650e15fe8
sudocodedev <shiva.r.160899@gmail.com>
2025-10-02 14:42:46 +0530

    removed locale support in helper fn

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,8 @@
-import locale
 import random
 from datetime import date, datetime, timedelta, timezone
 from typing import Optional, Tuple
 
 from src.settings import USER_AGENTS
-
-locale.setlocale(locale.LC_MONETARY, "en_IN")
 
 
 def user_agent() -> str:
@@ -29,10 +26,6 @@ def parse_date(data: str, format: str = "%d/%b/%Y") -> Optional[date]:
 
 def compute_percent(data1: float, data2: float) -> float:
     return round(((data1 - data2) / data2) * 100, 2)
-
-
-def format_currentcy(amount: float) -> str:
-    return locale.currency(amount, grouping=True)
 
 
 def ist_now():
